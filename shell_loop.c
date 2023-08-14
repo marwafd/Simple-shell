e564222b8647642dca69592b50b3524f6a1b16e4
marwafd <fadil.marwa17@gmail.com>
2023-08-14 06:49:20 -0700

    all files

--- shell_loop.c
+++ shell_loop.c
@@ -139,7 +139,7 @@ void fork_cmd(info_t *info)
 	{
 		if (execve(info->path, info->argv, get_environ(info)) == -1)
 		{
-			free_info(info, 1);
+			free_infos(info, 1);
 			if (errno == EACCES)
 				exit(126);
 			exit(1);
